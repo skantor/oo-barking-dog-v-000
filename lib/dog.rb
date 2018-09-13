dc8c498fb9a800d36cfa1e60c5db4ecefe3e7e16
Sara Kantor <sara.kantor.nv@gmail.com>
2018-09-13 18:21:55 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,14 @@
 # Your code goes here!
 class Dog
-  def initialize(breed)
-    @breed = breed
+  def initialize(name)
+    @name = name
   end
   
-  def name=(dogs_name)
-    @this_dogs_name = dogs_name
+  def name=(name)
+    @name = dogs_name
   end
  
   def name
-    @this_dogs_name
+    @name
   end 
 end
